f3fef171ce7c9d62c23283c19e9ae7b58f3d9a61
nigelb <nigel.blair+github@gmail.com>
2012-04-29 10:52:12 +1000

    Added copyright on new file.

--- src/main/java/jlibudev/monitor/ActionMultiplexer.java
+++ src/main/java/jlibudev/monitor/ActionMultiplexer.java
@@ -1,12 +1,31 @@
+/*
+* jlibudev provides JNA access to libudev.
+* Copyright (C) 2011 NigelB
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+
 package jlibudev.monitor;
 
 import jlibudev.UdevDevice;
 
 /**
- * Date: 29/04/12
- * Time: 10:37 AM
+ * <code>ActionMultiplexer</code> multiplexes the device action into the appropriate DeviceListener call.
  *
- * @author Nigel Bajema
+ * @author NigelB
  */
 public enum ActionMultiplexer {
     ADD("add"){
